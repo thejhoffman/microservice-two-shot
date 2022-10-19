4b16833e4932716bbdaa0c70163fb0fcfd1f7f68
Micheline <MDoughlin@hotmail.com>
2022-10-19 16:06:23 -0400

    Model Completed View Half

--- shoes/api/shoes_rest/models.py
+++ shoes/api/shoes_rest/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
 # Create your models here.
+class BinVO(models.Model):
+    closet_name = models.CharField(max_length=100)
+    import_href = models.CharField(max_length=200, unique=True)
+
+class Shoe(models.Model):
+    manufacturer = models.CharField(max_length=200)
+    model_name = models.CharField(max_length=200)
+    color = models.CharField(max_length=200)
+    picture_url = models.URLField(null=True)
+    bin = models.ForeignKey(
+        BinVO,
+        related_name="bin",
+        on_delete=models.PROTECT,
+    )
+
+    def _str_(self):
+        return self.name
--- shoes/api/shoes_rest/views.py
+++ shoes/api/shoes_rest/views.py
@@ -1,3 +1,36 @@
-from django.shortcuts import render
+from django.http import JsonResponse
+from .models import Shoe, BinVO
+import json
+from django.views.decorators.http import require_http_methods
+from common.json import ModelEncoder
 
-# Create your views here.
+
+class BinVOEncoder(ModelEncoder):
+    model = BinVO
+    properties = ["closet_name", "import_href"]
+
+class ShoeDetailEncoder(ModelEncoder):
+    model = Shoe
+    properties = [
+        "manufacturer",
+        "model_name",
+        "color"
+    ]
+    encoders = {
+        "bin": BinVOEncoder(),
+    }
+
+
+class ShoeListEncoder(ModelEncoder):
+    model = Shoe
+    properties =["model_name", "color"]
+
+
+@require_http_methods(["GET", "POST"])
+def api_list_shoes(request):
+    if request.method == "GET":
+            shoes = Shoe.objects.all()
+            return JsonResponse(
+                { "shoes":shoes},
+                encoder=ShoeListEncoder
+                )
