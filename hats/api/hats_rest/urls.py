09981a908a924707b78aac78680abc947356ff45
James Hoffman <jamesvhoffman@outlook.com>
2022-10-19 14:27:08 -0400

    Added missing backslash

--- hats/api/hats_rest/urls.py
+++ hats/api/hats_rest/urls.py
@@ -3,5 +3,5 @@ from .views import api_hat, api_hats
 
 urlpatterns = [
     path("hats/", api_hats, name="api_hats"),
-    path("hats/<int:pk>", api_hat, name="api_hat"),
+    path("hats/<int:pk>/", api_hat, name="api_hat"),
 ]
