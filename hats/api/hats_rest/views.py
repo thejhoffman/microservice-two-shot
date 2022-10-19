28688863eeb8b29f4893abdeb5304e99df245b63
James Hoffman <jamesvhoffman@outlook.com>
2022-10-19 13:51:48 -0400

    Created placeholder views

--- hats/api/hats_rest/views.py
+++ hats/api/hats_rest/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+# from django.http import JsonResponse
+from django.views.decorators.http import require_http_methods
 
-# Create your views here.
+# import json
+
+# import encoders
+# import models
+
+
+@require_http_methods(["GET", "POST"])
+def api_hats(request):
+    pass
+
+
+@require_http_methods(["GET", "PUT", "DELETE"])
+def api_hat(request, pk):
+    pass
