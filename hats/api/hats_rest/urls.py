25b8f14a14c70ae19f5460336095a8de2471a048
James Hoffman <jamesvhoffman@outlook.com>
2022-10-19 14:09:41 -0400

    Corrected path names

--- hats/api/hats_rest/urls.py
+++ hats/api/hats_rest/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from .views import api_hat, api_hats
 
 urlpatterns = [
-    path("locations/", api_hats, name="api_hats"),
-    path("locations/<int:pk>", api_hat, name="api_hat"),
+    path("hats/", api_hats, name="api_hats"),
+    path("hats/<int:pk>", api_hat, name="api_hat"),
 ]
