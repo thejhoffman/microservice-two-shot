1345c2c5e39ea3277ed211f1a96e137d45095d59
James Hoffman <jamesvhoffman@outlook.com>
2022-10-19 13:15:08 -0400

    Defined models and migrated

--- hats/api/hats_rest/models.py
+++ hats/api/hats_rest/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+
+class LocationVO(models.Model):
+    import_href = models.CharField(max_length=200, unique=True)
+    name = models.CharField(max_length=100)
+
+
+class Hat(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    location = models.ForeignKey(
+        LocationVO,
+        related_name="hats",
+        on_delete=models.CASCADE,
+    )
