2c5b69b59948cf63da3a3bd01639c9dd02a25695
James Hoffman <jamesvhoffman@outlook.com>
2022-10-19 12:22:17 -0400

    Add paths to urls.py

--- hats/api/hats_rest/urls.py
+++ hats/api/hats_rest/urls.py
@@ -1,5 +1,7 @@
-from django.urls import path, include
+from django.urls import path
+from .views import api_hat, api_hats
 
 urlpatterns = [
-    # paths here
+    path("locations/", api_hats, name="api_hats"),
+    path("locations/<int:pk>", api_hat, name="api_hat"),
 ]
