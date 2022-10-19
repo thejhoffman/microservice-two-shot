7ca216364b7849fd7b7af369ae3edda9349645dc
James Hoffman <jamesvhoffman@outlook.com>
2022-10-19 13:14:31 -0400

    Fixed urls misspelling

--- hats/api/hats_project/urls.py
+++ hats/api/hats_project/urls.py
@@ -18,5 +18,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("api/", include("hat_rest.urls")),
+    path("api/", include("hats_rest.urls")),
 ]
