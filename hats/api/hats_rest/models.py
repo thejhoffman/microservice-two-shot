748fbb2613df59ec96f161f5937c4c59e0b8b6a0
James Hoffman <jamesvhoffman@outlook.com>
2022-10-20 16:27:54 -0400

    Added __str__ methods to models

--- hats/api/hats_rest/models.py
+++ hats/api/hats_rest/models.py
@@ -6,6 +6,9 @@ class LocationVO(models.Model):
     import_href = models.CharField(max_length=200, unique=True)
     name = models.CharField(max_length=100)
 
+    def __str__(self):
+        return f"{self.name}"
+
 
 class Hat(models.Model):
     fabric = models.CharField(max_length=100)
@@ -17,6 +20,9 @@ class Hat(models.Model):
         related_name="hats",
         on_delete=models.CASCADE,
     )
+
+    def __str__(self):
+        return f"{self.style_name}"
 
     def get_api_url(self):
         return reverse("api_hat", kwargs={"pk": self.pk})
